3be76583c3854f7efd1c4a5eda0b4f4f9da8079c
Phillip Knoll <phillip@phkn.net>
2019-09-06 02:21:58 +0000

    possible fix for FB weirdness in JSON output format

---
+++ fbjsonfix.py
@@ -0,0 +1,12 @@
+# From: https://stackoverflow.com/questions/50008296/facebook-json-badly-encoded
+
+import json
+
+def parse_obj(obj):
+    for key in obj:
+        if isinstance(obj[key], str):
+            obj[key] = obj[key].encode('latin_1').decode('utf-8')
+        elif isinstance(obj[key], list):
+            obj[key] = list(map(lambda x: x if type(x) != str else x.encode('latin_1').decode('utf-8'), obj[key]))
+        pass
+    return obj
